d616afb52737a590923d19028c34b7028dc9fb3b
Uclo <ramdanizulfa23@gmail.com>
2026-04-04 11:35:05 +0800

    `Added RepositoryServiceProvider to providers list`

---
+++ app/Repositories/BoardingHouseRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Models\BoardingHouse;
+
+class BoardingHouseRepository implements BoardingHouseRepositoryInterface
+{
+    public function getAllBoardingHouses($search = null, $category = null, $city = null)
+    {
+        $query = BoardingHouse::query();
+
+        if ($search) {
+            $query->where('name', 'like', '%' . $search . '%');
+        }
+
+        if ($category) {
+            $query->whereHas('category', function ($q) use ($category) {
+                $q->where('slug', $category);
+            });
+        }
+
+        if ($city) {
+            $query->whereHas('city', function ($q) use ($city) {
+                $q->where('slug', $city);
+            });
+        }
+
+        return $query->get();
+    }
+
+    public function getPopularBoardingHouses($limit = 5)
+    {
+        return BoardingHouse::withCount('transactions')->orderBy('transactions_count', 'desc')->take($limit)->get();
+    }
+
+    public function getBoardingHouseByCitySlug($slug)
+    {
+        return BoardingHouse::whereHas('city', function ($q) use ($slug) {
+            $q->where('slug', $slug);
+        })->get();
+    }
+
+    public function getBoardingHouseByCategorySlug($slug)
+    {
+        return BoardingHouse::whereHas('category', function ($q) use ($slug) {
+            $q->where('slug', $slug);
+        })->get();
+    }
+
+    public function getBoardingHouseBySlug($slug)
+    {
+        return BoardingHouse::where('slug', $slug)->first();
+    }
+}
